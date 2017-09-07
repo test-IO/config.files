4d78f2a0d0b7337eab35aa2d16d8f4cd3fa3dc22
Jiayi Zheng <jiayi.zheng@test.io>
2017-09-07 22:15:57 +0200

    Add vim setup

--- raspberry_setup.sh
+++ raspberry_setup.sh
@@ -10,6 +10,15 @@ sudo apt-get install -y \
     wget \
     sl
 
+# vim setup
+# install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+# install NERDTree
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+# install color schemes
+git clone https://github.com/flazz/vim-colorschemes.git ~/.vim/bundle/vim-colorschemes
+
 # vimrc
 wget 
 
