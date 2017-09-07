0aa645e440e81bd454aecfb133e7ac529641278f
Jan Schwenzien <jan@general-scripting.com>
2017-09-07 22:38:30 +0200

    setup & update, first version for testing (#3)

---
+++ raspberry_update.sh
@@ -0,0 +1,8 @@
+# vimrc
+wget -O ~/.vimrc https://raw.githubusercontent.com/test-IO/config.files/master/configs/vimrc
+
+# tmux config
+wget -O ~/.tmux.conf https://raw.githubusercontent.com/test-IO/config.files/master/configs/tmux
+
+# bash_aliases
+wget -O ~/.bash_aliases https://raw.githubusercontent.com/test-IO/config.files/master/configs/bash_aliases
--- vim_setup.sh
+++ vim_setup.sh
@@ -6,6 +6,3 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 # install color schemes
 git clone https://github.com/flazz/vim-colorschemes.git ~/.vim/bundle/vim-colorschemes
-
-# vimrc
-wget -O ~/.vimrc https://raw.githubusercontent.com/test-IO/config.files/master/configs/vimrc
