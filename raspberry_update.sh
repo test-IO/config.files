8bd2193cc580da46ad040173206bb2d005e41120
Simon Lacroix <simon@miaou.be>
2017-09-12 19:09:33 +0200

    Auto reload tmux configuration when updating

--- raspberry_update.sh
+++ raspberry_update.sh
@@ -3,6 +3,7 @@ wget -O ~/.vimrc https://raw.githubusercontent.com/test-IO/config.files/master/c
 
 # tmux config
 wget -O ~/.tmux.conf https://raw.githubusercontent.com/test-IO/config.files/master/configs/tmux
+tmux source-file ~/.tmux.conf
 
 # bash_aliases
 wget -O ~/.bash_aliases https://raw.githubusercontent.com/test-IO/config.files/master/configs/bash_aliases
